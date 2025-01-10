8fbc18490a3abff0144e5fbcb5deafe49feea388
waltesour <song1399302254@gmail.com>
2025-01-10 07:22:41 +0000

    feat:support onnx and pth model infer

--- predict.py
+++ predict.py
@@ -1,3 +1,10 @@
+# @Author  : SONG SHUXIANG
+# @Time    : 2025/1/10
+# @Func    : STDC-Seg pth模型与onnx模型进行推理
+# @Usage    :
+#       修改pth模型和onnx模型所在路径
+#       修改验证数据集所在路径和保存结果所在路径
+
 from skimage import morphology
 import math
 import cv2
@@ -9,6 +16,7 @@ import torch.nn as nn
 import os
 import sys
 import torch.nn.functional as F
+import onnxruntime as ort
 sys.path.append('/home/inspur/workspace/SONGSX/STDC-Seg') # 以便正确导入models文件夹
 from models.model_stages import BiSeNet
 from transform import *
@@ -98,17 +106,7 @@ def prediect(respth='./pretrained', dspth='./data', backbone='CatNetSmall', scal
                     num_workers=n_workers,
                     drop_last=False)
 
-    n_classes = 3
-    print("backbone:", backbone)
-    net = BiSeNet(backbone=backbone, n_classes=n_classes,
-                  use_boundary_2=use_boundary_2, use_boundary_4=use_boundary_4,
-                  use_boundary_8=use_boundary_8, use_boundary_16=use_boundary_16,
-                  use_conv_last=use_conv_last)
-    net.load_state_dict(torch.load(respth))
-    net.cuda()
-    net.eval()
     os.system('rm -rf {}'.format(outpth))
-
     if not os.path.exists(outpth):
         os.makedirs(outpth)
     # kernel = np.ones((20, 20), np.uint8)
@@ -119,11 +117,24 @@ def prediect(respth='./pretrained', dspth='./data', backbone='CatNetSmall', scal
     color_map[0, 0] = 255
     color_map[0, 1] = 255
     color_map[0, 2] = 255
-
     skeleton_out_dir = os.path.join(outpth, 'skeleton')
-
     if not os.path.exists(skeleton_out_dir):
         os.makedirs(skeleton_out_dir)
+
+    # 加载pth模型
+    n_classes = 3
+    print("backbone:", backbone)
+    net = BiSeNet(backbone=backbone, n_classes=n_classes,
+                  use_boundary_2=use_boundary_2, use_boundary_4=use_boundary_4,
+                  use_boundary_8=use_boundary_8, use_boundary_16=use_boundary_16,
+                  use_conv_last=use_conv_last)
+    net.load_state_dict(torch.load(respth))
+    net.cuda()
+    net.eval()
+
+    # 加载onnx模型
+    output_onnx_path='/home/inspur/workspace/SONGSX/STDC-Seg/checkpoints/bbdd1211/pths/model_maxmIOU100_feat_out.onnx'
+    ort_session = ort.InferenceSession(output_onnx_path)
 
     with torch.no_grad():
         for i, (imgs, img_pth) in enumerate(dl):
@@ -132,18 +143,37 @@ def prediect(respth='./pretrained', dspth='./data', backbone='CatNetSmall', scal
             basename = os.path.basename(img_pth)
             N, C, H, W = imgs.size()
             # new_hw = [int(H * scale), int(W * scale)]
-            new_hw = [2048, 2048] # 输入尺寸大效果更好,比例要和训练图片比例一致效果好
-            new_imgs = F.interpolate(imgs, new_hw, mode='bilinear', align_corners=True)
-            new_imgs = new_imgs.cuda()
-            logits = net(new_imgs)[0] # [0]是经过 FFM - 8x 的结果
-            logits = F.interpolate(logits, [H, W], mode='bilinear')
+            new_hw = [2432, 2048] # 输入尺寸大效果更好,比例要和训练图片比例一致效果好
+            new_imgs_cpu = F.interpolate(imgs, new_hw, mode='bilinear', align_corners=True)
+
+            #获取 pth 模型推理结果
+            new_imgs = new_imgs_cpu.cuda()
+            logits = net(new_imgs) # [0]是经过 FFM - 8x 的结果
+            logits_numpy = logits[0]
+            logits = F.interpolate(logits_numpy, [H, W], mode='bilinear')
             logits = torch.softmax(logits, dim=1)   # 沿着类别维度,得到每个像素属于不同类别的概率分布
             preds = torch.argmax(logits, dim=1)     # 对于每个像素位置，选出概率最大的类别索引作为预测类别
             preds = preds.squeeze(dim=0)            # 如果preds有一个多余的单一维度（例如，形状为(1,H,W)），那么这一行会移除这个维度，使得形状变为(H,W)
             preds = preds.cpu().numpy().astype('uint8')
             skeleton = preds * 122
             skeleton_out_path = os.path.join(skeleton_out_dir, basename)
-            skeleton_out_path = skeleton_out_path[:-4]+'_res.jpg'
+            skeleton_out_path = skeleton_out_path[:-4]+'_pth.jpg'
+            cv2.imwrite(skeleton_out_path, skeleton)
+
+
+            # 获取 ONNX 推理结果
+            dummy_input_numpy2 = new_imgs_cpu.numpy()
+            logits = ort_session.run(None, {'img': dummy_input_numpy2})
+            logits_onnx = logits[0]
+            logits_onnx = torch.from_numpy(logits_onnx)
+            logits = F.interpolate(logits_onnx, [H, W], mode='bilinear')
+            logits = torch.softmax(logits, dim=1)   # 沿着类别维度,得到每个像素属于不同类别的概率分布
+            preds = torch.argmax(logits, dim=1)     # 对于每个像素位置，选出概率最大的类别索引作为预测类别
+            preds = preds.squeeze(dim=0)            # 如果preds有一个多余的单一维度（例如，形状为(1,H,W)），那么这一行会移除这个维度，使得形状变为(H,W)
+            preds = preds.cpu().numpy().astype('uint8')
+            skeleton = preds * 122
+            skeleton_out_path = os.path.join(skeleton_out_dir, basename)
+            skeleton_out_path = skeleton_out_path[:-4]+'_onnx.jpg'
             cv2.imwrite(skeleton_out_path, skeleton)
             
             print(i)
